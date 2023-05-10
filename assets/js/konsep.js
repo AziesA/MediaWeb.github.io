b450cfb3526ec484db357bd020086d2156a0173f
AziesA <abdazies99@gmail.com>
2023-05-11 03:37:09 +0800

    commit

--- assets/js/konsep.js
+++ assets/js/konsep.js
@@ -63,12 +63,15 @@ function empat() {
 
 // tahapan contoh soal
 let bgnya = document.querySelectorAll('.cth1');
-let bgnya1 = document.querySelectorAll('.krng');
+let bgnya1 = document.querySelectorAll('.cth2');
+let bgnya2 = document.querySelectorAll('.cth3');
 let jadi = document.querySelectorAll('.aau');
 let tmblnya = document.querySelector('.tmb_cth1');
-let tmblnya1 = document.querySelector('.tmb_pngrangan');
+let tmblnya1 = document.querySelector('.tmb_cth2');
+let tmblnya2 = document.querySelector('.tmb_cth3');
 let kondisi1 = 0;
 let kondisi2 = 0;
+let kondisi3 = 0;
 tmblnya.addEventListener('click', function () {
     if (kondisi1 == 0) {
         kondisi1 += 1;
@@ -89,15 +92,47 @@ tmblnya1.addEventListener('click', function () {
     if (kondisi2 == 0) {
         kondisi2 += 1;
         bgnya1[0].className = bgnya1[0].className.replace('hilang1', '');
-        tmblnya1.innerText = "Kumpulkan Suku Sejenis";
+        tmblnya1.innerText = "Jawab";
     } else if (kondisi2 == 1) {
         kondisi2 += 1;
         bgnya1[1].className = bgnya1[1].className.replace('hilang1', '');
-        tmblnya1.innerText = "Operasikan Suku Sejenis";
+        tmblnya1.innerText = "Jawab";
     } else if (kondisi2 == 2) {
+        kondisi2 += 1;
         bgnya1[2].className = bgnya1[2].className.replace('hilang1', '');
+        tmblnya1.innerText = "Hasil";
+    } else if (kondisi2 == 3) {
         bgnya1[3].className = bgnya1[3].className.replace('hilang1', '');
         tmblnya1.className += ' hilang1';
         jadi[3].className = jadi[3].className.replace('hilang1', '');
+    }
+})
+
+
+tmblnya2.addEventListener('click', function () {
+    if (kondisi3 == 0) {
+        kondisi3 += 1;
+        bgnya2[0].className = bgnya2[0].className.replace('hilang1', '');
+        tmblnya2.innerText = "Jawab";
+    } else if (kondisi3 == 1) {
+        kondisi3 += 1;
+        bgnya2[1].className = bgnya2[1].className.replace('hilang1', '');
+        tmblnya2.innerText = "Jawab";
+    } else if (kondisi3 == 2) {
+        kondisi3 += 1;
+        bgnya2[2].className = bgnya2[2].className.replace('hilang1', '');
+        tmblnya2.innerText = "Jawab";
+    } else if (kondisi3 == 3) {
+        kondisi3 += 1;
+        bgnya2[3].className = bgnya2[3].className.replace('hilang1', '');
+        tmblnya2.innerText = "Jawab";
+    } else if (kondisi == 4) {
+        kondisi3 += 1;
+        bgnya2[4].className = bgnya2[4].className.replace('hilang1', '');
+        tmblnya2.innerText = "Hasil";
+    } else if (kondisi3 == 5) {
+        bgnya2[5].className = bgnya2[5].className.replace('hilang1', '');
+        tmblnya2.className += ' hilang1';
+        jadi[5].className = jadi[5].className.replace('hilang1', '');
     }
 })
