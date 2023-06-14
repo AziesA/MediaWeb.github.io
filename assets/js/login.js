a8dc5c72e25ebc5a4beb85474405c55a70a23c18
AziesA <abdazies99@gmail.com>
2023-06-14 13:09:54 +0800

    commit

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,7 +3,7 @@ function loginguru() {
     let password = document.getElementById("password").value;
 
     if (username === "guru1" && password === "guru1") {
-        location.href = "halamanguru/dashboard.html";
+        window.location.href = "http://127.0.0.1:5500/halamanguru/dashboard.html";
     } else {
         alert("Masukkan ID dan password yang benar.");
     }
